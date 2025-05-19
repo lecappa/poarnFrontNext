e50f1415a840137891a4aeee444ca176ec39d3ed
Guillaume Lesureur <lecappa@gmail.com>
2025-05-19 17:15:51 +0200

    Add Feats

--- pages/character/competences.vue
+++ pages/character/competences.vue
@@ -2,6 +2,7 @@
   <div class="row d-flex align-items-stretch">
     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 align-items-stretch">
       <character-other-skills-mastery></character-other-skills-mastery>
+      <character-gifts></character-gifts>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 align-items-stretch">
       <character-skills-mastery></character-skills-mastery>
